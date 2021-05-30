f6d72553124a3d10bfc50020aa64ac52832f62c3
yeahme49 <58564160+yeahme49@users.noreply.github.com>
2021-05-30 01:02:02 -0500

    use hls proxy for d+ us and change xbmc.translatepath to xbmcvfs.translatepath (#51)
    * use hls proxy to change framerate on discoveryplus.com
    
    * change xbmc.translatepath to xbmcvfs.translatepath

---
+++ services.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import threading
+
+import resources.service.proxy as proxy
+import pseudotv_recommended as pseudotv
+
+proxy_th = threading.Thread(target=proxy.main)
+psuedo_th = threading.Thread(target=pseudotv.regPseudoTV)
+
+proxy_th.start()
+psuedo_th.start()
